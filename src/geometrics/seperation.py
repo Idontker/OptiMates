4432e24402823604f88d37158455650c36f5be21
idontker <idontker@users.noreply.github.com>
2021-06-25 15:23:56 +0200

    moved and finished seperation modul

--- src/geometrics/seperation.py
+++ src/geometrics/seperation.py
@@ -1,10 +1,10 @@
 from numpy.core.function_base import linspace
 import src.graph.visu_sol as vis
 import numpy as np
-import math
+import logging
 
 
-def gen_bauer_spiral(N):
+def _gen_bauer_spiral(N):
     L = np.sqrt(N * np.pi)
     k = np.array(range(1, N + 1))
     z = 1 - (2 * k - 1) / N
@@ -15,7 +15,7 @@ def gen_bauer_spiral(N):
     return x, y, z
 
 
-def select_strip(arr, upper_z, lower_z, epsi=0):
+def _select_strip(arr, upper_z, lower_z, epsi=0):
     """ je upper_z, lower_z und epsi in rad"""
 
     smaller_than_upper = arr[2] <= np.cos(upper_z)
@@ -28,21 +28,21 @@ def select_strip(arr, upper_z, lower_z, epsi=0):
     return arr[:, within_bounds]
 
 
-def get_strips(arr, Z, epsi):
+def _get_strips(arr, Z, epsi):
     s = []
     M = len(Z)
     lens = []
 
     for i in range(M - 1):
-        tmp = select_strip(arr, Z[i], Z[i + 1], epsi)
+        tmp = _select_strip(arr, Z[i], Z[i + 1], epsi)
         lens.append(len(tmp[0]))
         s.append(tmp)
 
-    print(lens)
+    logging.info(lens)
     return s
 
 
-def new_gen(step_prct, epsi=0):
+def _new_gen(step_prct, epsi=0):
     """ 0 < step_prct < 1 """
 
     Z = np.array([0])
@@ -58,7 +58,7 @@ def new_gen(step_prct, epsi=0):
     return Z
 
 
-def validate_Z(Z, epsi=0):
+def _validate_Z(Z, epsi=0):
 
     faces = []
     for i in range(len(Z) - 1):
@@ -66,17 +66,24 @@ def validate_Z(Z, epsi=0):
         max = Z[i + 1] + epsi if Z[i + 1] + epsi < np.pi else np.pi
         face = np.cos(low) - np.cos(max)
         faces.append(face)
-    print("faces[0]:", faces[0], "avg(faces)", np.average(faces))
+    logging.info("faces[0]:", faces[0], "avg(faces)", np.average(faces))
 
     for i in range(len(Z) - 1):
         d = Z[i + 1] - Z[i] - epsi
         if d < -1e-11:
-            print(
+            logging.error(
                 "i:{}\tZ[i+1]-Z[i] - epsi >0 ? {}\t Z[i+1]-Z[i] - epsi = {}".format(
                     i, d > -1e-11, d
                 )
             )
     pass
+
+
+def create_stripes(points, epsi, step_prct):
+    Z = _new_gen(step_prct, epsi)
+    _validate_Z(Z, epsi)
+
+    return _get_strips(arr, Z, epsi)
 
 
 if __name__ == "__main__":
@@ -85,19 +92,18 @@ if __name__ == "__main__":
     step_prct = 0.5
     # r = 1.75
     r = 13
-    epsi = np.deg2rad(2 * r)
-    M = math.floor(180 / epsi)
+    epsi = np.deg2rad(r)
 
-    Z = new_gen(step_prct, epsi)
-    validate_Z(Z, epsi)
+    Z = _new_gen(step_prct, epsi)
+    _validate_Z(Z, epsi)
 
     # print(Z)
     # print("epsi: ", 2 * r, " deg", "  =  ", epsi, "rad")
 
-    x, y, z = gen_bauer_spiral(N)
+    x, y, z = _gen_bauer_spiral(N)
     arr = np.array([x, y, z])
 
-    strips = get_strips(arr, Z, epsi)
+    strips = _get_strips(arr, Z, epsi)
 
     ##########
     ## PLOT ##
